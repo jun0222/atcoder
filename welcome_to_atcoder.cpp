4ee2544ed420aaa281fda0fce09f1d5aa7b3ad0a
jun0222 <nextseason6@gmail.com>
2019-09-06 23:57:15 +0900

    first commit

---
+++ welcome_to_atcoder.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() a{
+  int a;
+	int b;
+	int c;
+	int sum;
+
+	short s;
+
+  cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> s;
+
+	sum = a + b + c;
+	cout << sum s;
+
+	return 0;
+}
