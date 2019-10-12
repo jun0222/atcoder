3403f84f6320ac42df45322313017801d9a51385
jun0222 <nextseason6@gmail.com>
2019-10-12 17:19:06 +0900

    --no-edit

--- 110.cpp
+++ 110.cpp
@@ -7,16 +7,19 @@ int main() {
   // ここにプログラムを追記
 
 	int i = 0;
-
+  cout << "A:";
   while (i < A) {
     cout << "]";
     i++;
   }
   cout << endl;
-
+  
+	i = 0;
+	cout << "B:";
   while (i < B) {
     cout << "]";
     i++;
   }
+  cout << endl;
 }
 
